9228902f463152fae2e6271263ca78baa8d6300c
lifespanpt <49885190+lifespanpt@users.noreply.github.com>
2019-04-22 15:28:13 -0700

    Add files via upload

--- TestPatientWontWork.js
+++ TestPatientWontWork.js
@@ -27,6 +27,12 @@ p.nominalBounds = new cjs.Rectangle(0,0,190,50);
 p.nominalBounds = new cjs.Rectangle(0,0,84,42);
 
 
+(lib.ScreenShot20190422at32710PM = function() {
+	this.initialize(img.ScreenShot20190422at32710PM);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,91,30);
+
+
 (lib.ScreenShot20190422at74532AMpngcopy = function() {
 	this.initialize(img.ScreenShot20190422at74532AMpngcopy);
 }).prototype = p = new cjs.Bitmap();
@@ -527,6 +533,10 @@ p.nominalBounds = new cjs.Rectangle(-45,-19,93,36);
 	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(1).call(this.frame_1).wait(1).call(this.frame_2).wait(1).call(this.frame_3).wait(1).call(this.frame_4).wait(1).call(this.frame_5).wait(1).call(this.frame_6).wait(1).call(this.frame_7).wait(1).call(this.frame_8).wait(1).call(this.frame_9).wait(2));
 
 	// actions
+	this.instance = new lib.ScreenShot20190422at32710PM();
+	this.instance.parent = this;
+	this.instance.setTransform(18,675);
+
 	this.text = new cjs.Text("Click Retrive to open the patient record", "24px 'Times'", "#6633CC");
 	this.text.textAlign = "center";
 	this.text.lineHeight = 26;
@@ -534,9 +544,9 @@ p.nominalBounds = new cjs.Rectangle(-45,-19,93,36);
 	this.text.parent = this;
 	this.text.setTransform(477.5,106);
 
-	this.instance = new lib.ScreenShot20190422at83613AMpngcopy();
-	this.instance.parent = this;
-	this.instance.setTransform(0,1);
+	this.instance_1 = new lib.ScreenShot20190422at83613AMpngcopy();
+	this.instance_1.parent = this;
+	this.instance_1.setTransform(0,1);
 
 	this.Scrolldown1 = new lib.ScrollDown();
 	this.Scrolldown1.name = "Scrolldown1";
@@ -544,8 +554,8 @@ p.nominalBounds = new cjs.Rectangle(-45,-19,93,36);
 	this.Scrolldown1.setTransform(536.5,726.1);
 	new cjs.ButtonHelper(this.Scrolldown1, 0, 1, 1);
 
-	this.instance_1 = new lib.ScreenShot20190422at74532AMpngcopy();
-	this.instance_1.parent = this;
+	this.instance_2 = new lib.ScreenShot20190422at74532AMpngcopy();
+	this.instance_2.parent = this;
 
 	this.HandP = new lib.HP();
 	this.HandP.name = "HandP";
@@ -553,18 +563,18 @@ p.nominalBounds = new cjs.Rectangle(-45,-19,93,36);
 	this.HandP.setTransform(81,224.45);
 	new cjs.ButtonHelper(this.HandP, 0, 1, 1);
 
-	this.instance_2 = new lib.Retrive();
-	this.instance_2.parent = this;
-	this.instance_2.setTransform(-0.5,-251.9);
-	new cjs.ButtonHelper(this.instance_2, 0, 1, 1);
-
-	this.instance_3 = new lib.ScreenShot20190422at75450AM();
+	this.instance_3 = new lib.Retrive();
 	this.instance_3.parent = this;
-	this.instance_3.setTransform(0,203);
+	this.instance_3.setTransform(-0.5,-251.9);
+	new cjs.ButtonHelper(this.instance_3, 0, 1, 1);
 
-	this.instance_4 = new lib.ScreenShot20190422at75123AM();
+	this.instance_4 = new lib.ScreenShot20190422at75450AM();
 	this.instance_4.parent = this;
-	this.instance_4.setTransform(1,1);
+	this.instance_4.setTransform(0,203);
+
+	this.instance_5 = new lib.ScreenShot20190422at75123AM();
+	this.instance_5.parent = this;
+	this.instance_5.setTransform(1,1);
 
 	this.Return1 = new lib.Return();
 	this.Return1.name = "Return1";
@@ -572,8 +582,8 @@ p.nominalBounds = new cjs.Rectangle(-45,-19,93,36);
 	this.Return1.setTransform(73,87.45);
 	new cjs.ButtonHelper(this.Return1, 0, 1, 1);
 
-	this.instance_5 = new lib.ScreenShot20190422at75854AM();
-	this.instance_5.parent = this;
+	this.instance_6 = new lib.ScreenShot20190422at75854AM();
+	this.instance_6.parent = this;
 
 	this.Scrolly = new lib.Scroll_1();
 	this.Scrolly.name = "Scrolly";
@@ -581,9 +591,9 @@ p.nominalBounds = new cjs.Rectangle(-45,-19,93,36);
 	this.Scrolly.setTransform(535,729.9);
 	new cjs.ButtonHelper(this.Scrolly, 0, 1, 1);
 
-	this.instance_6 = new lib.HP("synched",0);
-	this.instance_6.parent = this;
-	this.instance_6.setTransform(81,224.45);
+	this.instance_7 = new lib.HP("synched",0);
+	this.instance_7.parent = this;
+	this.instance_7.setTransform(81,224.45);
 
 	this.otherformbutton1 = new lib.OtherForms();
 	this.otherformbutton1.name = "otherformbutton1";
@@ -591,8 +601,8 @@ p.nominalBounds = new cjs.Rectangle(-45,-19,93,36);
 	this.otherformbutton1.setTransform(207.95,581.3);
 	new cjs.ButtonHelper(this.otherformbutton1, 0, 1, 1);
 
-	this.instance_7 = new lib.ScreenShot20190422at80722AM();
-	this.instance_7.parent = this;
+	this.instance_8 = new lib.ScreenShot20190422at80722AM();
+	this.instance_8.parent = this;
 
 	this.Selectform = new lib.Selectform();
 	this.Selectform.name = "Selectform";
@@ -600,9 +610,9 @@ p.nominalBounds = new cjs.Rectangle(-45,-19,93,36);
 	this.Selectform.setTransform(301.95,375.4);
 	new cjs.ButtonHelper(this.Selectform, 0, 1, 1);
 
-	this.instance_8 = new lib.ScreenShot20190422at81131AM();
-	this.instance_8.parent = this;
-	this.instance_8.setTransform(0,-1);
+	this.instance_9 = new lib.ScreenShot20190422at81131AM();
+	this.instance_9.parent = this;
+	this.instance_9.setTransform(0,-1);
 
 	this.TO = new lib.TherapyOrder();
 	this.TO.name = "TO";
@@ -610,9 +620,9 @@ p.nominalBounds = new cjs.Rectangle(-45,-19,93,36);
 	this.TO.setTransform(285.4,428.35);
 	new cjs.ButtonHelper(this.TO, 0, 1, 2, false, new lib.TherapyOrder(), 3);
 
-	this.instance_9 = new lib.ScreenShot20190422at81402AM();
-	this.instance_9.parent = this;
-	this.instance_9.setTransform(1,0);
+	this.instance_10 = new lib.ScreenShot20190422at81402AM();
+	this.instance_10.parent = this;
+	this.instance_10.setTransform(1,0);
 
 	this.Confirm1 = new lib.Confirm();
 	this.Confirm1.name = "Confirm1";
@@ -620,17 +630,17 @@ p.nominalBounds = new cjs.Rectangle(-45,-19,93,36);
 	this.Confirm1.setTransform(273.5,434);
 	new cjs.ButtonHelper(this.Confirm1, 0, 1, 1);
 
-	this.instance_10 = new lib.ScreenShot20190422at81814AM();
-	this.instance_10.parent = this;
-	this.instance_10.setTransform(1,2);
+	this.instance_11 = new lib.ScreenShot20190422at81814AM();
+	this.instance_11.parent = this;
+	this.instance_11.setTransform(1,2);
 
 	this.SchedulingTOText = new lib.an_TextInput({'id': 'SchedulingTOText', 'value':'', 'disabled':false, 'visible':true, 'class':'ui-textinput'});
 
 	this.SchedulingTOText.setTransform(313,475.9,6,3,0,0,0,50,11);
 
-	this.instance_11 = new lib.ScreenShot20190422at82200AM();
-	this.instance_11.parent = this;
-	this.instance_11.setTransform(-7,2,0.9787,1);
+	this.instance_12 = new lib.ScreenShot20190422at82200AM();
+	this.instance_12.parent = this;
+	this.instance_12.setTransform(-7,2,0.9787,1);
 
 	this.SchedulingTO = new cjs.Text("", "24px 'Times'");
 	this.SchedulingTO.name = "SchedulingTO";
@@ -640,7 +650,7 @@ p.nominalBounds = new cjs.Rectangle(-45,-19,93,36);
 	this.SchedulingTO.parent = this;
 	this.SchedulingTO.setTransform(314,441.95);
 
-	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance},{t:this.text,p:{x:477.5,y:106,text:"Click Retrive to open the patient record",lineWidth:300}}]}).to({state:[{t:this.instance_1},{t:this.text,p:{x:169,y:410.95,text:"Here you will see the patient's contact information",lineWidth:313}},{t:this.Scrolldown1}]},1).to({state:[{t:this.instance_4},{t:this.instance_3},{t:this.text,p:{x:366.5,y:180.05,text:"Click the Uploaded Documents to review the H&P!",lineWidth:300}},{t:this.instance_2},{t:this.HandP}]},1).to({state:[{t:this.instance_5},{t:this.text,p:{x:389.85,y:369.85,text:"This is where you'll find alternative contact info and medical history",lineWidth:436}},{t:this.Return1}]},1).to({state:[{t:this.instance_4},{t:this.instance_3},{t:this.instance_6},{t:this.text,p:{x:414.85,y:157.95,text:"Now lets put in the scheduling therapy order!",lineWidth:406}},{t:this.Scrolly}]},1).to({state:[{t:this.instance_7},{t:this.otherformbutton1},{t:this.text,p:{x:380.9,y:571.8,text:"Choose Other Forms ",lineWidth:228}}]},1).to({state:[{t:this.instance_8},{t:this.text,p:{x:399.85,y:233.95,text:"Click on 'Select Form Type' to bring up the options.",lineWidth:372}},{t:this.Selectform}]},1).to({state:[{t:this.instance_9},{t:this.text,p:{x:437.85,y:263.9,text:"Select Therapy Order",lineWidth:280}},{t:this.TO}]},1).to({state:[{t:this.instance_10},{t:this.text,p:{x:437,y:280.05,text:"Finally, choose Confirm",lineWidth:340}},{t:this.Confirm1}]},1).to({state:[{t:this.instance_11},{t:this.text,p:{x:309,y:565.9,text:"The Scheduling Therapy Order should be formatted as:\n\n'spoke with (relationship) (name) to schedule (discipline) Evaluation for (date). (reason why family may have delayed Eval, if applicable).' ",lineWidth:582}},{t:this.SchedulingTOText}]},1).to({state:[{t:this.instance_11},{t:this.SchedulingTO},{t:this.text,p:{x:309,y:565.9,text:"The Scheduling Therapy Order should be formatted as:\n\n'spoke with (relationship) (name) to schedule (discipline) Evaluation for (date). (reason why family may have delayed Eval, if applicable).' ",lineWidth:582}}]},1).wait(1));
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_1},{t:this.text,p:{x:477.5,y:106,text:"Click Retrive to open the patient record",lineWidth:300}},{t:this.instance}]}).to({state:[{t:this.instance_2},{t:this.text,p:{x:169,y:410.95,text:"Here you will see the patient's contact information",lineWidth:313}},{t:this.Scrolldown1}]},1).to({state:[{t:this.instance_5},{t:this.instance_4},{t:this.text,p:{x:366.5,y:180.05,text:"Click the Uploaded Documents to review the H&P!",lineWidth:300}},{t:this.instance_3},{t:this.HandP}]},1).to({state:[{t:this.instance_6},{t:this.text,p:{x:389.85,y:369.85,text:"This is where you'll find alternative contact info and medical history",lineWidth:436}},{t:this.Return1}]},1).to({state:[{t:this.instance_5},{t:this.instance_4},{t:this.instance_7},{t:this.text,p:{x:414.85,y:157.95,text:"Now lets put in the scheduling therapy order!",lineWidth:406}},{t:this.Scrolly}]},1).to({state:[{t:this.instance_8},{t:this.otherformbutton1},{t:this.text,p:{x:380.9,y:571.8,text:"Choose Other Forms ",lineWidth:228}}]},1).to({state:[{t:this.instance_9},{t:this.text,p:{x:399.85,y:233.95,text:"Click on 'Select Form Type' to bring up the options.",lineWidth:372}},{t:this.Selectform}]},1).to({state:[{t:this.instance_10},{t:this.text,p:{x:437.85,y:263.9,text:"Select Therapy Order",lineWidth:280}},{t:this.TO}]},1).to({state:[{t:this.instance_11},{t:this.text,p:{x:437,y:280.05,text:"Finally, choose Confirm",lineWidth:340}},{t:this.Confirm1}]},1).to({state:[{t:this.instance_12},{t:this.text,p:{x:309,y:565.9,text:"The Scheduling Therapy Order should be formatted as:\n\n'spoke with (relationship) (name) to schedule (discipline) Evaluation for (date). (reason why family may have delayed Eval, if applicable).' ",lineWidth:582}},{t:this.SchedulingTOText}]},1).to({state:[{t:this.instance_12},{t:this.SchedulingTO},{t:this.text,p:{x:309,y:565.9,text:"The Scheduling Therapy Order should be formatted as:\n\n'spoke with (relationship) (name) to schedule (discipline) Evaluation for (date). (reason why family may have delayed Eval, if applicable).' ",lineWidth:582}}]},1).wait(1));
 
 	// Layer_1
 	this.retrieve1 = new lib.Retrive();
@@ -662,29 +672,30 @@ lib.properties = {
 	color: "#FFFFFF",
 	opacity: 1.00,
 	manifest: [
-		{src:"images/button1.png?1555971382015", id:"button1"},
-		{src:"images/button.png?1555971382015", id:"button"},
-		{src:"images/ScreenShot20190419at20450PMpngcopy.png?1555971382015", id:"ScreenShot20190419at20450PMpngcopy"},
-		{src:"images/ScreenShot20190422at74532AMpngcopy.png?1555971382015", id:"ScreenShot20190422at74532AMpngcopy"},
-		{src:"images/ScreenShot20190422at75123AM.png?1555971382015", id:"ScreenShot20190422at75123AM"},
-		{src:"images/ScreenShot20190422at75413AM.png?1555971382015", id:"ScreenShot20190422at75413AM"},
-		{src:"images/ScreenShot20190422at75450AM.png?1555971382015", id:"ScreenShot20190422at75450AM"},
-		{src:"images/ScreenShot20190422at75854AM.png?1555971382015", id:"ScreenShot20190422at75854AM"},
-		{src:"images/ScreenShot20190422at80722AM.png?1555971382015", id:"ScreenShot20190422at80722AM"},
-		{src:"images/ScreenShot20190422at80838AM.png?1555971382015", id:"ScreenShot20190422at80838AM"},
-		{src:"images/ScreenShot20190422at81131AM.png?1555971382015", id:"ScreenShot20190422at81131AM"},
-		{src:"images/ScreenShot20190422at81151AM.png?1555971382015", id:"ScreenShot20190422at81151AM"},
-		{src:"images/ScreenShot20190422at81402AM.png?1555971382015", id:"ScreenShot20190422at81402AM"},
-		{src:"images/ScreenShot20190422at81415AM.png?1555971382015", id:"ScreenShot20190422at81415AM"},
-		{src:"images/ScreenShot20190422at81645AM.png?1555971382015", id:"ScreenShot20190422at81645AM"},
-		{src:"images/ScreenShot20190422at81814AM.png?1555971382015", id:"ScreenShot20190422at81814AM"},
-		{src:"images/ScreenShot20190422at81823AM.png?1555971382015", id:"ScreenShot20190422at81823AM"},
-		{src:"images/ScreenShot20190422at82200AM.png?1555971382015", id:"ScreenShot20190422at82200AM"},
-		{src:"images/ScreenShot20190422at83613AMpngcopy.png?1555971382015", id:"ScreenShot20190422at83613AMpngcopy"},
-		{src:"images/Scroll.png?1555971382015", id:"Scroll"},
-		{src:"https://code.jquery.com/jquery-2.2.4.min.js?1555971382015", id:"lib/jquery-2.2.4.min.js"},
-		{src:"components/sdk/anwidget.js?1555971382015", id:"sdk/anwidget.js"},
-		{src:"components/ui/src/textinput.js?1555971382015", id:"an.TextInput"}
+		{src:"images/button1.png", id:"button1"},
+		{src:"images/button.png", id:"button"},
+		{src:"images/ScreenShot20190419at20450PMpngcopy.png", id:"ScreenShot20190419at20450PMpngcopy"},
+		{src:"images/ScreenShot20190422at32710PM.png", id:"ScreenShot20190422at32710PM"},
+		{src:"images/ScreenShot20190422at74532AMpngcopy.png", id:"ScreenShot20190422at74532AMpngcopy"},
+		{src:"images/ScreenShot20190422at75123AM.png", id:"ScreenShot20190422at75123AM"},
+		{src:"images/ScreenShot20190422at75413AM.png", id:"ScreenShot20190422at75413AM"},
+		{src:"images/ScreenShot20190422at75450AM.png", id:"ScreenShot20190422at75450AM"},
+		{src:"images/ScreenShot20190422at75854AM.png", id:"ScreenShot20190422at75854AM"},
+		{src:"images/ScreenShot20190422at80722AM.png", id:"ScreenShot20190422at80722AM"},
+		{src:"images/ScreenShot20190422at80838AM.png", id:"ScreenShot20190422at80838AM"},
+		{src:"images/ScreenShot20190422at81131AM.png", id:"ScreenShot20190422at81131AM"},
+		{src:"images/ScreenShot20190422at81151AM.png", id:"ScreenShot20190422at81151AM"},
+		{src:"images/ScreenShot20190422at81402AM.png", id:"ScreenShot20190422at81402AM"},
+		{src:"images/ScreenShot20190422at81415AM.png", id:"ScreenShot20190422at81415AM"},
+		{src:"images/ScreenShot20190422at81645AM.png", id:"ScreenShot20190422at81645AM"},
+		{src:"images/ScreenShot20190422at81814AM.png", id:"ScreenShot20190422at81814AM"},
+		{src:"images/ScreenShot20190422at81823AM.png", id:"ScreenShot20190422at81823AM"},
+		{src:"images/ScreenShot20190422at82200AM.png", id:"ScreenShot20190422at82200AM"},
+		{src:"images/ScreenShot20190422at83613AMpngcopy.png", id:"ScreenShot20190422at83613AMpngcopy"},
+		{src:"images/Scroll.png", id:"Scroll"},
+		{src:"https://code.jquery.com/jquery-2.2.4.min.js", id:"lib/jquery-2.2.4.min.js"},
+		{src:"components/sdk/anwidget.js", id:"sdk/anwidget.js"},
+		{src:"components/ui/src/textinput.js", id:"an.TextInput"}
 	],
 	preloads: []
 };
